dca96dcd273dc728ac7ed7de5027acc4f52cfd25
Michaeldq1 <michaeldquinton@gmail.com>
2023-01-11 14:27:06 -0600

    plugging currently functionality into input type range element

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const board = document.getElementById('board');
-const button = document.querySelector('.boxes');
-let numberOfBoxes = 3;
+const rangeSlider = document.querySelector('input');
+console.log(rangeSlider.value);
 
-button.addEventListener('click', () => {
+rangeSlider.addEventListener('change', () => {
+    let numberOfBoxes = rangeSlider.value;
     board.textContent = '';
-    for (let i = 0; i < numberOfBoxes * numberOfBoxes; i++) {
+    for (let i = 0; i < (numberOfBoxes * numberOfBoxes); i++) {
         let box = document.createElement('div');
         box.style.width = `${board.clientWidth / numberOfBoxes}px`;
         box.style.height = `${board.clientHeight / numberOfBoxes}px`;
         board.appendChild(box);
+        console.log(numberOfBoxes);
+        console.log(board.clientWidth);
+        console.log(board.clientHeight);
+
     }
-})
+});
