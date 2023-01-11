a9c4c7e5de4dd221ce70621c514d47f3f26e1a04
Michaeldq1 <michaeldquinton@gmail.com>
2023-01-11 08:16:02 -0600

    functionality to populate a set number of boxes on the board

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const board = document.getElementById('board');
 const button = document.querySelector('.boxes');
-let numberOfBoxes = 16;
+const boardWidth = 400;
+const boardHeight = 400;
+let numberOfBoxes = 8;
 
 button.addEventListener('click', () => {
     for (let i = 0; i < numberOfBoxes * numberOfBoxes; i++) {
-        let box = document.createElement('div');
+        let box = document.createElement('div');1
+        box.style.width = `${boardWidth / numberOfBoxes}px`;
+        box.style.height = `${boardWidth / numberOfBoxes}px`;
         board.appendChild(box);
         console.log(i);
     }
