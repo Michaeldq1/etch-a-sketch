29a50bbc289c2e840e5205ecbf7d83dfc4929e9d
Michaeldq1 <michaeldquinton@gmail.com>
2023-01-11 08:26:34 -0600

    fix to remove existing boxes on board to be replaced with new boxes when the button is clicked

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const board = document.getElementById('board');
 const button = document.querySelector('.boxes');
 const boardWidth = 400;
 const boardHeight = 400;
-let numberOfBoxes = 8;
+let numberOfBoxes = 40;
 
 button.addEventListener('click', () => {
+    board.textContent = '';
     for (let i = 0; i < numberOfBoxes * numberOfBoxes; i++) {
         let box = document.createElement('div');1
         box.style.width = `${boardWidth / numberOfBoxes}px`;
         box.style.height = `${boardWidth / numberOfBoxes}px`;
         board.appendChild(box);
-        console.log(i);
     }
 })
