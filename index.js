ba1e0a7506daccc0bdfc829a1b9e84e2632e24a7
Michaeldq1 <michaeldquinton@gmail.com>
2023-01-11 15:25:07 -0600

    Load event to populate boxes when window is loaded. Function to populate boxes

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const board = document.getElementById('board');
 const rangeSlider = document.querySelector('input');
-console.log(rangeSlider.value);
 
-rangeSlider.addEventListener('change', () => {
+window.addEventListener('load', generateBoxes);
+rangeSlider.addEventListener('change', generateBoxes);
+
+function generateBoxes() {
     let numberOfBoxes = rangeSlider.value;
     board.textContent = '';
+
     for (let i = 0; i < (numberOfBoxes * numberOfBoxes); i++) {
         let box = document.createElement('div');
         box.style.width = `${board.clientWidth / numberOfBoxes}px`;
         box.style.height = `${board.clientHeight / numberOfBoxes}px`;
         board.appendChild(box);
-        console.log(numberOfBoxes);
-        console.log(board.clientWidth);
-        console.log(board.clientHeight);
-
     }
-});
+}
