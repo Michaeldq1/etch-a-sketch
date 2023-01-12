140fbed427b9abc4e6af9bba90281d43c0f24620
Michaeldq1 <michaeldquinton@gmail.com>
2023-01-11 20:27:17 -0600

    functionality of shape radio buttons

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const board = document.getElementById('board');
-const rangeSlider = document.querySelector('input');
+const rangeSlider = document.querySelector('.slider');
+const circleOption = document.getElementById('circle');
+const squareOption = document.getElementById('square');
 
 window.addEventListener('load', generateBoxes);
+
 rangeSlider.addEventListener('change', generateBoxes);
+
+squareOption.addEventListener('click', generateBoxes);
+
+circleOption.addEventListener('click', changeShape);
 
 function generateBoxes() {
     let numberOfBoxes = rangeSlider.value;
@@ -11,7 +18,15 @@ function generateBoxes() {
     for (let i = 0; i < (numberOfBoxes * numberOfBoxes); i++) {
         let box = document.createElement('div');
         box.style.width = `${board.clientWidth / numberOfBoxes}px`;
-        box.style.height = `${board.clientHeight / numberOfBoxes}px`;
+        box.style.height = `${board.clientHeight / numberOfBoxes}px`;       
         board.appendChild(box);
+    }
+}
+
+function changeShape(){
+    let boxes = document.querySelectorAll('#board div');
+    Array.from(boxes);
+    for (const box of boxes) {
+        box.classList.toggle('circles', circleOption.checked);
     }
 }
