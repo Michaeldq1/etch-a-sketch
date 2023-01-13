6c55ab44d4f4e6dc3761598cfaa851e4ce0de3fd
Michaeldq1 <michaeldquinton@gmail.com>
2023-01-12 20:08:47 -0600

    functionality for rainbow, eraser, and clear buttons

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const colorModeButton = document.getElementById('color-mode');
 const colorPicker = document.getElementById('color-picker');
 const rainbowModeButton = document.getElementById('rainbow-mode');
 const eraserButton = document.getElementById('eraser');
+const clearButton = document.getElementById('clear');
+
+let colorMode = false;
+let rainbowMode = false;
+let eraserMode = false;
 
 window.addEventListener('DOMContentLoaded', () => {
     enableColorMode();
@@ -26,8 +31,7 @@ eraserButton.addEventListener('click', enableEraser);
 
 colorPicker.addEventListener('change', changeColor);
 
-
-  
+clearButton.addEventListener('click', clearBoard);
 
 function generateBoxes() {
     let numberOfBoxes = boxSizeSlider.value;
@@ -37,8 +41,7 @@ function generateBoxes() {
         let box = document.createElement('div');
         box.style.width = `${board.clientWidth / numberOfBoxes}px`;
         box.style.height = `${board.clientHeight / numberOfBoxes}px`;
-        box.classList.add('box');
-        changeShape();     
+        box.classList.add('box');  
         board.appendChild(box);
     }
 
@@ -61,18 +64,54 @@ function enableColorMode() {
     colorModeButton.style.border = '2px solid black';
     rainbowModeButton.style.border = '';
     eraserButton.style.border = '';
+    colorMode = true;
+    rainbowMode = false;
+    eraserMode = false;
+    board.style.border = `2px solid ${colorPicker.value}`;
+    changeColor(colorPicker.value);
+    colorBoxes();
 }
 
 function enableRainbowMode() {
     colorModeButton.style.border = '';
     rainbowModeButton.style.border = '2px solid black';
     eraserButton.style.border = '';
+    colorMode = false;
+    rainbowMode = true;
+    eraserMode = false;
+    colorBoxes();
 }
 
 function enableEraser() {
     colorModeButton.style.border = '';
     rainbowModeButton.style.border = '';
     eraserButton.style.border = '2px solid black';
+    colorMode = false;
+    rainbowMode = false;
+    eraserMode = true;
+    colorBoxes();
+}
+
+function colorBoxes() {
+    const boxes = document.querySelectorAll('#board div');
+
+    boxes.forEach((element) => {
+        if (colorMode === true) {
+            element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = `${colorPicker.value}`;
+            });
+        } else if (rainbowMode === true) {
+            const randomColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+            element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = `${randomColor}`;
+            });
+        } else if (eraserMode === true) {
+            let eraserColor = board.style.backgroundColor;
+            element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = eraserColor;
+            });
+        }
+    });
 }
 
 function changeColor() {
@@ -86,12 +125,7 @@ function changeColor() {
     }
 }
 
-function colorBoxes() {
-    const boxes = document.querySelectorAll('#board div');
-
-    boxes.forEach((element) => {
-        element.addEventListener('mouseover', () => {
-            element.style.backgroundColor = `${colorPicker.value}`;
-        });
-    })
+function clearBoard() {
+    board.textContent = '';
+    generateBoxes();
 }
